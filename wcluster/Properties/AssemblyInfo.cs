ed4f42d7c7f369d89958f7f7bf94b897937eb440
斟酌 鵬兄 <tgckpg@gmail.com>
2016-03-02 02:01:24 +0800

    Add some info

--- wcluster/Properties/AssemblyInfo.cs
+++ wcluster/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCompany( "" )]
 [assembly: AssemblyProduct( "wcluster" )]
-[assembly: AssemblyCopyright( "Copyright ©  2016" )]
+[assembly: AssemblyCopyright( "Astropenguin © 2016" )]
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
 
